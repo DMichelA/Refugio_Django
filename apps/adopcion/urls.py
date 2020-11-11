fb3e5bb960be5f6aa6038b57913ed3e93858a468
Amairani Michel Duran <50429279+DMichelA@users.noreply.github.com>
2020-11-11 01:43:14 -0600

    Delete and Update 2 Forms

--- apps/adopcion/urls.py
+++ apps/adopcion/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path,include
 
-from apps.adopcion.views import SolicitudList, SolicitudCreate
+from apps.adopcion.views import SolicitudList, SolicitudCreate, SolicitudUpdate, SolicitudDelete
 
 urlpatterns = [
     path('solicitud/listar/', SolicitudList.as_view(), name="solicitud_listar"),
     path('solicitud/nueva/', SolicitudCreate.as_view(), name="solicitud_crear"),
+    path('solicitud/editar/<int:pk>', SolicitudUpdate.as_view(), name="solicitud_editar"),
+    path('solicitud/eliminar/<int:pk>', SolicitudDelete.as_view(), name="solicitud_eliminar"),
 ]
--- apps/mascota/urls.py
+++ apps/mascota/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', index, name="index"),
     path('nuevo/', MascotaCreate.as_view(), name="mascota_crear"),
     path('listar/', MascotaList.as_view(), name="mascota_listar"),
-    path('editar/<int:pk>/', MascotaUpdate.as_view(), name="mascota_editar"), #Expresion regular para recibir un parametro
-    path('eliminar/<int:pk>/', MascotaDelete.as_view(), name="mascota_eliminar"), #Cuando es vista basa en clase se cambia el parametro que pasara a pk en lugar de id_mascota
+    path('editar/<int:pk>', MascotaUpdate.as_view(), name="mascota_editar"), #Expresion regular para recibir un parametro
+    path('eliminar/<int:pk>', MascotaDelete.as_view(), name="mascota_eliminar"), #Cuando es vista basa en clase se cambia el parametro que pasara a pk en lugar de id_mascota
 ]
