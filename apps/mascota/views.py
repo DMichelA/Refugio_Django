96edee3f8956adabdc61615dc8181ac7fa01b67e
Amairani Michel Duran <50429279+DMichelA@users.noreply.github.com>
2020-11-11 00:10:25 -0600

    CRUD Operable

--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from apps.mascota.forms import MascotaForm
 from apps.mascota.models import Mascota
@@ -58,4 +58,9 @@ class MascotaUpdate(UpdateView):
     model = Mascota
     form_class = MascotaForm
     template_name = 'mascota/mascota_form.html'
+    success_url = reverse_lazy('mascota_listar')
+
+class MascotaDelete(DeleteView):
+    model = Mascota
+    template_name = 'mascota/mascota_delete.html'
     success_url = reverse_lazy('mascota_listar')
