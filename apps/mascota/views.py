73492919cb2af67ffe0f749753b12e2b1dbf2072
Amairani Michel Duran <50429279+DMichelA@users.noreply.github.com>
2020-11-12 00:45:58 -0600

    Add Pagination

--- apps/mascota/views.py
+++ apps/mascota/views.py
@@ -46,7 +46,7 @@ def mascota_delete(request, id_mascota):
 class MascotaList(ListView):
     model = Mascota
     template_name = 'mascota/mascota_list.hmtl'
-
+    paginate_by = 5
 
 class MascotaCreate(CreateView):
     model = Mascota
